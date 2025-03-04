5cf74dc4f8ce9bd33f370daf8a39b1d0270aa4ac
ledutu2 <tung.le@saigontechnology.com>
2025-03-04 15:24:26 +0700

    feat: update testing setup, add unit tests, and enhance CI workflows

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,25 @@
 module.exports = {
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-};
+  preset: 'jest-expo',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/e2e/', '/__tests__/providers/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|' +
+      '@react-native(-community)?)|' +
+      'expo(nent)?|@expo(nent)?/.*|' +
+      '@expo-google-fonts/.*|' +
+      'react-navigation|' +
+      '@react-navigation/.*|' +
+      '@sentry/react-native|' +
+      'native-base|' +
+      'rn-base-component|' +
+      'react-native-svg)',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'json-summary', 'text', 'text-summary'],
+  reporters: ['default', ['jest-junit', {outputDirectory: 'coverage'}]],
+  verbose: true,
+  moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node', 'mts', 'cts', 'svg'],
+  coveragePathIgnorePatterns: [],
+}
