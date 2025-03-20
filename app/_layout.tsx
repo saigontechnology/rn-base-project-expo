753cd9855da15cdefdd61824b74657df4ecf8286
DanhMC <danh.mai@saigontechnology.com>
2025-03-20 17:30:35 +0700

    fix: add MainLayout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,57 +1,18 @@
-import {configureLocalization} from '@/locale/I18nConfig'
 import {persistor, store} from '@/stores/store'
-import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native'
-import {useFonts} from 'expo-font'
-import * as SplashScreen from 'expo-splash-screen'
-import {StatusBar} from 'expo-status-bar'
-import React, {useEffect} from 'react'
-import {StyleSheet, useColorScheme} from 'react-native'
+import React from 'react'
+import {StyleSheet} from 'react-native'
 import 'react-native-reanimated'
 import {Provider} from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react'
-import {injectStore} from '@/services/networking/axios'
-import {BaseProvider} from 'rn-base-component'
-import {theme} from '@/themes'
-import {BottomSheetModalProvider} from '@gorhom/bottom-sheet'
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
-import {RootNavigation} from '@/routes/AppNavigation'
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync()
-
-injectStore(store)
-configureLocalization('vi')
+import MainLayout from '@/screens/MainLayout'
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
-  const [loaded] = useFonts({
-    RobotoRegular: require('../src/assets/fonts/Roboto-Regular.ttf'),
-    RobotoMedium: require('../src/assets/fonts/Roboto-Medium.ttf'),
-    RobotoBold: require('../src/assets/fonts/Roboto-Bold.ttf'),
-  })
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync()
-    }
-  }, [loaded])
-
-  if (!loaded) {
-    return null
-  }
-
   return (
     <GestureHandlerRootView style={styles.flex}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BaseProvider theme={theme}>
-            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-              <BottomSheetModalProvider>
-                <RootNavigation />
-                <StatusBar style="auto" />
-              </BottomSheetModalProvider>
-            </ThemeProvider>
-          </BaseProvider>
+          <MainLayout />
         </PersistGate>
       </Provider>
     </GestureHandlerRootView>
---
+++ src/screens/MainLayout.tsx
@@ -0,0 +1,57 @@
+import {configureLocalization} from '@/locale/I18nConfig'
+import {store} from '@/stores/store'
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native'
+import {useFonts} from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import {StatusBar} from 'expo-status-bar'
+import React, {useEffect} from 'react'
+import {useColorScheme} from 'react-native'
+import 'react-native-reanimated'
+import {injectStore} from '@/services/networking/axios'
+import {BaseProvider} from 'rn-base-component'
+import {theme} from '@/themes'
+import {BottomSheetModalProvider} from '@gorhom/bottom-sheet'
+import {RootNavigation} from '@/routes/AppNavigation'
+import {useSelector} from 'react-redux'
+import {getLoadingIndicator} from '@/stores/selectors'
+import {IndicatorDialog} from '@/components'
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync()
+
+injectStore(store)
+configureLocalization('en')
+
+export default function MainLayout() {
+  const colorScheme = useColorScheme()
+  const showGlobalIndicator = useSelector(getLoadingIndicator)
+  const [loaded] = useFonts({
+    RobotoRegular: require('../src/assets/fonts/Roboto-Regular.ttf'),
+    RobotoMedium: require('../src/assets/fonts/Roboto-Medium.ttf'),
+    RobotoBold: require('../src/assets/fonts/Roboto-Bold.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <>
+      <BaseProvider theme={theme}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <BottomSheetModalProvider>
+            <RootNavigation />
+            <StatusBar style="auto" />
+          </BottomSheetModalProvider>
+        </ThemeProvider>
+      </BaseProvider>
+      {showGlobalIndicator && <IndicatorDialog />}
+    </>
+  )
+}
